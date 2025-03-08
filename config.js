632361cda2b8aa578ffb8821e422515f1f8fef73
𝑅𝛩𝛭𝛯𝛩 𝐵𝑊 𝛪𝛪𝛪 <161124347+romeobwiii@users.noreply.github.com>
2025-03-08 13:47:38 +0200

    Update config.js

--- config.js
+++ config.js
@@ -26,6 +26,6 @@ GOOGLE_CX: process.env.GOOGLE_CX || "AIzaSyD93IeJsouK51zjKgyHAwBIAlqr-a8mnME",
 PASTEBIN_API_KEY: process.env.PASTEBIN_API_KEY || "uh8QvO6vQJGtIug9WvjdTAPx_ZAFJAxn",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 ///=================ඕවා එඩිට් කරද්දි කිසිම දෙයක් වැරදියට දමන්න එපා !!ALIVE-CONFIG================================
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/gdvaaw.jpg", //add alive img url
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/rrz7rs.jpeg", //add alive img url
 ALIVE_MSG: process.env.ALIVE_MSG || "HI AM WHATSAPP BOT", //add alive msg
 };
